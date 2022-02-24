c9c9262c76da20bb8a7d03f71508ca32c48f59c8
yasserAbdelkader98 <yasser.abdelkader2020@gmail.com>
2022-02-24 15:43:56 +0200

    workspace schema
    last updated with mohamed erd

--- Models/WorkspaceSchema.js
+++ Models/WorkspaceSchema.js
@@ -1,12 +1,15 @@
 const mongoose = require("mongoose");
 
 const WorkspaceSchema = new mongoose.Schema({
-  name: { type: String, maxlength: 30, required: true },
-  admin: { type: mongoose.Types.ObjectId, ref: "User", required: true },
-  date_created: { type: Date, required: true },
+  title: { type: String, maxlength: 30, required: true },
   description: { type: String, maxlength: 200 },
-  channels: [{ type: mongoose.Types.ObjectId, ref: "Channel", required: true }],
+  owner: { type: mongoose.Types.ObjectId, ref: "User", required: true },
   members: [{ type: mongoose.Types.ObjectId, ref: "User", required: true }],
+  direct_messages: { type: mongoose.Types.ObjectId, ref: "Channel" },
+  channels: [ { type: mongoose.Types.ObjectId, ref: "Channel", required: true }],
+  booked_offline_offices: Object,
+  invitations: [ { type: mongoose.Types.ObjectId, ref:"User" }],
+  date_Created: { type: Date, required: true },
 });
 
 module.exports = mongoose.model("WorkSpace", WorkspaceSchema);
