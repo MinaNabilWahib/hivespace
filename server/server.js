eac6f8888058dd9f6ac29529d28bc643569bb892
Mina Nabil <minanabilsalib@gmail.com>
2022-02-24 17:39:17 +0200

    rearchritecture

--- server/server.js
+++ server/server.js
@@ -1,19 +1,16 @@
-const express = require("express");
+const express = require('express')
 // const morgan = require('morgan');
 // const cors = require("cors");
 // const body_parser = require("body-parser");
 // const mongoose = require("mongoose");
 
-
-
 //create server
-const app = express();
+const app = express()
 
 //listen on port number
 app.listen(process.env.PORT || 8080, () => {
-    console.log("I am live and listening...");
-    // console.log(process.env.NODE_MODE);
-});
+  console.log('I am live and listening...')
+  // console.log(process.env.NODE_MODE);
+})
 
-
-//todo middlewares 
+//todo middlewares
