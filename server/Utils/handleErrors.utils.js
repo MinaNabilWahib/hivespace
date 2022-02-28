a5371f460c27ea62c8b7fd6991359cbef45396a9
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-03-01 01:42:42 +0200

    dealing with errors

---
+++ server/Utils/handleErrors.utils.js
@@ -0,0 +1,20 @@
+const { validationResult } = require("express-validator");
+
+exports.handleErrors = (req, res, next) => {
+    let errors = validationResult(req);
+    console.log(errors);
+    if (!errors.isEmpty()) {
+        let error = new Error();
+        error.status = 422;
+        error.message = errors.array().reduce((current, object) => current + object.msg + " ", "")
+        throw error;
+    }
+    next()
+}//handle express validator error
+
+exports.generateError = (status, message) => {
+    let error = new Error();
+    error.status = status;
+    error.message = message;
+    throw error;
+}//generate new error
