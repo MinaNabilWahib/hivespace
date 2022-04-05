dbb0b35561efe01086dd8b80b37bdbdc658a2b89
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-04-05 22:23:28 +0200

    HTTPS config
    HTTPS with node , self signed certificates and public key cryptography

--- server/server.js
+++ server/server.js
@@ -1,55 +1,73 @@
-require("dotenv").config();
-const express = require("express");
-const helmet = require("helmet");
-const morgan = require('morgan');
-const body_parser = require("body-parser");
-const connectToDb = require("./Config/db");
-const path = require("path");
-const authRouter = require('./Routers/authRouter');
+require('dotenv').config()
+const fs = require('fs')
+const express = require('express')
+const helmet = require('helmet')
+const morgan = require('morgan')
+const body_parser = require('body-parser')
+const connectToDb = require('./Config/db')
+const path = require('path')
+const authRouter = require('./Routers/authRouter')
 
 //create server
-const app = express();
-
+const app = express()
+//https
+const https = require('https')
 //secure headers
-app.use(helmet());
+app.use(helmet())
 //use morgan
-app.use(morgan(':method :url :status :http-version :response-time '));
+app.use(morgan(':method :url :status :http-version :response-time '))
 
 // allow cross origin
 app.use((req, res, next) => {
-  res.header("Access-Control-Allow-Origin", "*");
-  res.header("Access-Control-Allow-Methods", "GET,POST,DELETE,PUT,OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type,Authorization")
-  next();
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Methods', 'GET,POST,DELETE,PUT,OPTIONS')
+  res.header('Access-Control-Allow-Headers', 'Content-Type,Authorization')
+  next()
 })
 
 //image upload
-app.use("/media", express.static(path.join(__dirname, "Media")));
+app.use('/media', express.static(path.join(__dirname, 'Media')))
 
 // body parser
-app.use(body_parser.json());
-app.use(body_parser.urlencoded({ extended: true }));
+app.use(body_parser.json())
+app.use(body_parser.urlencoded({ extended: true }))
 
 //listening to port 8000
-const port = process.env.PORT || 8000;
-app.listen(port, async () => {
-  await connectToDb();
-  console.log(`server running on ${port}`)
-});
+const port = process.env.PORT || 8000
+// app.listen(port, async () => {
+//   await connectToDb();
+//   console.log(`server running on ${port}`)
+// });
+https
+  .createServer(
+    {
+      key: fs.readFileSync('key.pem'),
+      cert: fs.readFileSync('cert.pem'),
+    },
+    app,
+  )
+  .listen(port, async () => {
+    try {
+      await connectToDb()
+      console.log(`server running on ${port}`)
+    } catch (error) {
+      console.log(error)
+    }
+  })
 
 //put routes
-app.use(authRouter);
+app.use(authRouter)
 
 //Not found MW
 app.use((req, res) => {
-  res.status(404).json({ data: "Not Found" });
+  res.status(404).json({ data: 'Not Found' })
 })
 
 //Error MW
 app.use((error, req, res, next) => {
   try {
-    let status = error.status || 500;
-    res.status(status).json({ Error: `${error}` });
+    let status = error.status || 500
+    res.status(status).json({ Error: `${error}` })
   } catch (error) {
     next(error)
   }
