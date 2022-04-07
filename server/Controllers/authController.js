837d0e9a81c472b7613ff61711a06a02f7178b01
Mohamed Sayed <mohamed.sayed.atiaa@gmail.com>
2022-04-07 23:11:51 +0200

    working on registration route

--- server/Controllers/authController.js
+++ server/Controllers/authController.js
@@ -1,10 +1,14 @@
+const jwt = require('jsonwebtoken')
 const User = require('../Models/UserSchema')
+const sendEmail = require('../Service/emailTransporter')
+const { generateError } = require('../Utils/handleErrors.utils')
 
 exports.register_post = async (req, res, next) => {
   try {
     //get data from body
-    const { first_name, last_name, email, password, passwordConfirm, country } = req.body
+    const { first_name, last_name, email, password, passwordConfirm, country, phone_number } = req.body
     let user = {}
+    console.log(phone_number, req.phoneNumber + '');
     user = User({
       first_name,
       last_name,
@@ -12,16 +16,58 @@ exports.register_post = async (req, res, next) => {
       password,
       passwordConfirm,
       country,
-      phone_number: req.phoneNumber,
+      phone_number: req.phoneNumber + '',
       image: '',
     })
-
     //save data
-    // await user.save();
-    req.user = user
-    res.status(200).json({ user })
-    // next();
+    await user.save();
+    req.user = user.userData();
+    next();
   } catch (error) {
     next(error)
   }
-} //registration for speakers
+} //registration for users
+
+
+exports.sendVerificationEmail = async (req, res, next) => {
+  try {
+    const infoHash = {};
+    const user = req.user;
+    infoHash.user = user;
+    infoHash.id = user._id;
+    console.log(user);
+    const key = eval(process.env.mail_key);
+    const token = jwt.sign(infoHash, key, { expiresIn: "24h" });
+    const link = `${process.env.BASE_URL}/user/verify/${user._id}/${token}`;
+    //generate html code
+    const html = `<h3 style="color:blue;">Hello, ${user.fullName}</h3>
+    <p>E-mail verification was requested for this email address ${user.email}. If you requested this verification, click the link below :</p>
+    <p>
+      <a style="background-color:blue; color:white;padding:10px 20px;text-decoration:none; font-weight:bold;border-radius:7px" href="${link}">Verify Your Email</a>
+    </p>`
+    await sendEmail(user.email, "Verify Email", html);
+    res.status(201).json({ data: "Registration successful ,An Email sent to your account please verify", token });
+  } catch (error) {
+    next(error)
+  }
+}//send email verification
+
+exports.emailVerify = async (req, res, next) => {
+  try {
+    const key = process.env.mail_key;
+    const user = await userVerify(req, key);
+    await user.update({ verified: true })
+    res.status(200).json("mail verified success")
+  } catch (error) {
+    next(error)
+  }
+}//verify email on link sent
+
+const userVerify = async (req, key) => {
+  const user = await User.findById(req.params.id);
+  if (!user) { generateError(400, "invalid link") }
+  const token = jwt.verify(req.params.token, eval(key));
+  if (!token) { generateError(400, "invalid link") }
+  return user;
+}//token and user verify
+
--- server/Routers/authRouter.js
+++ server/Routers/authRouter.js
@@ -1,10 +1,19 @@
 const express = require('express')
-const { register_post } = require('../Controllers/authController')
+const { register_post, sendVerificationEmail, emailVerify } = require('../Controllers/authController')
 const { registerValidation } = require('../Service/authValidation')
+const { emailValidation } = require('../Service/userDataValidation')
 const { handleErrors } = require('../Utils/handleErrors.utils')
 const router = express.Router()
 
-router.post('/register', registerValidation, handleErrors, register_post)
+//register route 
+router.post('/register', registerValidation, handleErrors, register_post, sendVerificationEmail);
+
+//re send E-mail verify
+router.post("/verifyEmail", emailValidation, handleErrors, sendVerificationEmail);
+
+// E-mail verify
+router.get("/user/verify/:id/:token", emailVerify);
+
 router.get('/me', (req, res) => {
   res.status(200).json({ data: 'dsadfsdf' })
 })
