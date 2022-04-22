829d0ffb6a4dc44a8e3159201c38c473f7c374ab
muradtag <murad.tag.97@gmail.com>
2022-04-22 04:25:56 +0200

    add socket utils

--- server/Utils/socketio.utils.js
+++ server/Utils/socketio.utils.js
@@ -1,9 +1,12 @@
+const DailyChat = require('../Models/DailyChatSchema')
+const Channels = require('../Models/ChannelSchema')
+
 //current users in server.
 const users = []
 
 // Join user to chat
-function userJoin(socketId, userId, username, room) {
-  const user = { socketId, userId, username, room }
+function userJoin(socketId, userId, room) {
+  const user = { socketId, userId, room }
   users.push(user)
   return user
 }
@@ -27,13 +30,51 @@ function getRoomUsers(room) {
   return users.filter(user => user.room === room)
 }
 
-function formatMessage(userId, username, text) {
+function formatMessage(userId, text) {
   return {
-    userId,
-    username,
+    sender: userId,
     data: text,
-    date_created: new Date().getTime(),
+    timestamp: new Date().getTime(),
   }
+}
+
+//get messages from db
+function getMessagesDB(channelId, day, cb) {
+  DailyChat.findOne({ _id: `${channelId}_${day}` })
+    .populate('messages.sender', 'first_name last_name image')
+    .exec((err, data) => {
+      if (err) console.log(err)
+      cb(data)
+    })
+}
+
+// save message to db
+function saveMessageDB(message, channelId, cb) {
+  console.log(message.timestamp)
+  let day = new Date(message.timestamp).setHours(0, 0, 0, 0)
+  console.log(day)
+  day = new Date(day).toLocaleDateString()
+
+  console.log(day)
+
+  DailyChat.findOneAndUpdate({ _id: `${channelId}_${day}` }, { $push: { messages: message } }, { upsert: true })
+    .then(() => {
+      return Channels.findOneAndUpdate(
+        { _id: channelId },
+        { $addToSet: { messages: `${channelId}_${day}` } },
+        { upsert: true },
+      )
+    })
+    .then(() => {
+      DailyChat.findOne({ _id: `${channelId}_${day}` }, 'messages -_id')
+        .select({ messages: { $elemMatch: { timestamp: `${message.timestamp}` } } })
+        .populate('messages.sender', 'first_name last_name image')
+        .exec((err, data) => {
+          if (err) console.log(err)
+          cb(data)
+        })
+    })
+    .catch(error => console.log(error))
 }
 
 module.exports = {
@@ -42,4 +83,6 @@ module.exports = {
   userLeave,
   getRoomUsers,
   formatMessage,
+  saveMessageDB,
+  getMessagesDB,
 }
